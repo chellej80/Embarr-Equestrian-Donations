2e5933f1d1ebe7e67050fef0602409460f8ffefc
chellej80 <embarrequestrian@gmail.com>
2022-10-25 19:40:21 +0000

    updated setting py with static and media

--- embarr_donations/settings.py
+++ embarr_donations/settings.py
@@ -146,5 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
