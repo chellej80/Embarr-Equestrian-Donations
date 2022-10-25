482309afd09b867acadce9a720008e23dbeef538
chellej80 <embarrequestrian@gmail.com>
2022-10-25 22:22:35 +0000

    updated animals admin py with two admin classes for animal catalog and categories to make the admin interface more intuitive

--- animals/admin.py
+++ animals/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Animal, Category
 
 # Register your models here.
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'donate',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
 admin.site.register(Animal)
 admin.site.register(Category)
