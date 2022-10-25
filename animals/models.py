1cafa3d0d1f5f37a9721604dbe666858fb5f5678
chellej80 <embarrequestrian@gmail.com>
2022-10-25 22:16:35 +0000

    loaded fixture data to create animal catalog and categories in admin, tested and sucessful

--- animals/models.py
+++ animals/models.py
@@ -17,7 +17,7 @@ class Animal(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    Donate = models.DecimalField(max_digits=6, decimal_places=2)
+    donate = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
