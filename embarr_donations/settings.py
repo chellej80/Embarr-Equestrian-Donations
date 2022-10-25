c414bc7be7a5799c275ae736d1f715c005db2d1e
chellej80 <embarrequestrian@gmail.com>
2022-10-25 20:23:29 +0000

    created mobile and main nav html in includes folder

--- embarr_donations/settings.py
+++ embarr_donations/settings.py
@@ -150,3 +150,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- embarr_donations/urls.py
+++ embarr_donations/urls.py
@@ -15,13 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-]from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
