a5d63cddf4ece9c0e490e817e812f906e3d8dd6b
chellej80 <embarrequestrian@gmail.com>
2022-10-26 15:00:14 +0000

    updated the base html and nav html to action the get function for the search bar tool

--- animals/urls.py
+++ animals/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_animals, name='animals')
+    path('', views.all_animals, name='animals'),
     path('<animal_id>', views.animal_detail, name='animal_detail'),
 ]
--- animals/views.py
+++ animals/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Animal
 
 # Create your views here.
@@ -18,7 +18,7 @@ def all_animals(request):
 def animal_detail(request, animal_id):
     """ A view to show individual product details """
 
-    animal = get_object_or_404(Product, pk=animal_id)
+    animal = get_object_or_404(Animal, pk=animal_id)
 
     context = {
         'animal': animal,
