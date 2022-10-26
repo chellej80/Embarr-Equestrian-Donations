fc674c02787d319ce8750a31237620257fadf328
chellej80 <embarrequestrian@gmail.com>
2022-10-26 15:09:03 +0000

    updated the animal app views py with search query get function

--- animals/views.py
+++ animals/views.py
@@ -8,9 +8,21 @@ def all_animals(request):
     """ A view to show all products, including sorting and search queries """
 
     animals = Animal.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('animals'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'animals': animals,
+        'search_term': query,
     }
 
     return render(request, 'animals/animals.html', context)
