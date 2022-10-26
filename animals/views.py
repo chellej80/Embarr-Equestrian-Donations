a340fd766ebde0d2ac31e9f4b31b287ff62dd678
chellej80 <embarrequestrian@gmail.com>
2022-10-26 14:34:23 +0000

    updated views in animal with aninmal details def

--- animals/views.py
+++ animals/views.py
@@ -14,3 +14,14 @@ def all_animals(request):
     }
 
     return render(request, 'animals/animals.html', context)
+
+def animal_detail(request, animal_id):
+    """ A view to show individual product details """
+
+    animal = get_object_or_404(Product, pk=animal_id)
+
+    context = {
+        'animal': animal,
+    }
+
+    return render(request, 'animals/animal_detail.html', context)
