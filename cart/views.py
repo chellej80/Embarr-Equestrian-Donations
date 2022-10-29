baea943c5534103b314adc7f9d73f741bd40d856
chellej80 <embarrequestrian@gmail.com>
2022-10-29 15:51:12 +0000

    created simple cart view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_cart(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'cart/cart.html')
