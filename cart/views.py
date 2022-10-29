e02550068792542db0fbc9b9c917974eb2d22e6c
chellej80 <embarrequestrian@gmail.com>
2022-10-29 16:45:02 +0000

    updated views py in cart app with add to cart function

--- cart/views.py
+++ cart/views.py
@@ -6,3 +6,19 @@ def view_cart(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
