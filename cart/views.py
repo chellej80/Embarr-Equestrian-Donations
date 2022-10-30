ccf3781fb2c93ae17a2cc250c2ba548500918588
chellej80 <embarrequestrian@gmail.com>
2022-10-30 15:05:25 +0000

    updated cart view importing messages

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse, \
                              HttpResponse, get_object_or_404
-
+from django.contrib import messages
 # Create your views here.
 
 
