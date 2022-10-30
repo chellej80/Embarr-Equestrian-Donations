6d5108fb373493379919e75ccbd26add803f5e96
chellej80 <embarrequestrian@gmail.com>
2022-10-30 13:46:05 +0000

    updated the cart views with function to remove cart items, updated the JS for the cart html also

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, reverse, \
+                             HttpResponse, get_object_or_404
 
 # Create your views here.
 
@@ -30,12 +31,24 @@ def adjust_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
-        if quantity > 0:
-            cart[item_id] = quantity
-        else:
-            cart.pop(item_id)
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping bag"""
 
+    try:
+        
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
