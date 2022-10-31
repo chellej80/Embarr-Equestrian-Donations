b38740aa8f3c5d5c030fd591bc4d6808ba73dc64
chellej80 <embarrequestrian@gmail.com>
2022-10-31 22:27:54 +0000

    updated view in checkout order form is valid

--- checkout/views.py
+++ checkout/views.py
@@ -50,6 +50,10 @@ def checkout(request):
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
             for item_id, item_data in cart.items():
                 try:
                     animal = Animal.objects.get(id=item_id)
