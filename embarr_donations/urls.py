14b8268c4c30b6887e0821b1b9c7faacbec222ea
chellej80 <embarrequestrian@gmail.com>
2022-10-31 10:04:42 +0000

    created the checkout checkout urls and added the url to the proj level file

--- embarr_donations/urls.py
+++ embarr_donations/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('animals/', include('animals.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
