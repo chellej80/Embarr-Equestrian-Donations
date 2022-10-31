15a9d9e28be4be833cfc8830ab09964d74576e14
chellej80 <embarrequestrian@gmail.com>
2022-10-31 21:32:55 +0000

    created webhooks py, updated settings & added webhooks url

---
+++ checkout/webhooks.py
@@ -0,0 +1,41 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from checkout.webhook_handler import StripeWH_Handler
+
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """
+    Function to listen for webhooks from Stripe
+    """
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+                payload, sig_header, wh_secret)
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
+
+    handler = StripeWH_Handler(request)
+
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_failed,
+    }
+
+    event_type = event['type']
+    event_handler = event_map.get(event_type, handler.handle_event)
+    response = event_handler(event)
+    return response
