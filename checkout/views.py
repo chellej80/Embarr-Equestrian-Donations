20ec7ef5e5399e40dbfd824f02c4d9007b416759
chellej80 <embarrequestrian@gmail.com>
2022-10-31 11:49:09 +0000

    updated checkout view with cart contexts import and stripe total func

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
 
 
 def checkout(request):
@@ -10,6 +11,10 @@ def checkout(request):
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('animals'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
