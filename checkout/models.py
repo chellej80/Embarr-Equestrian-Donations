6da5e55400bcf07fd98fa2401a5cbd081ad5bbe1
chellej80 <embarrequestrian@gmail.com>
2022-11-01 09:17:50 +0000

    imported country field in order model for checkout

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django_countries.fields import CountryField
 
 from animals.models import Animal
 
