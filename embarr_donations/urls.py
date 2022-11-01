c5e5843b1f443620ca0913bdd820f79d15be409d
chellej80 <embarrequestrian@gmail.com>
2022-11-01 09:34:52 +0000

    created the profles html

--- embarr_donations/urls.py
+++ embarr_donations/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('animals/', include('animals.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
