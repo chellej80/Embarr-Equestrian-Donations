d2dc8e8d987c66cecae6e2947e9769a766af85f9
chellej80 <embarrequestrian@gmail.com>
2022-11-02 14:36:04 +0000

    udpated the main nav html

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('home/', views.index, name='home'),
+    path('about', views.about, name='about')
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/about.html')
