3a45098e2d217eec0ead9d97010cf2349566094f
chellej80 <embarrequestrian@gmail.com>
2022-11-24 22:01:51 +0000

    installed reqs, made migrations & loaded data after repository restart

--- embarr_donations/settings.py
+++ embarr_donations/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["embarr-donations.herokuapp.com", "localhost"]
 
@@ -121,15 +121,22 @@ WSGI_APPLICATION = "embarr_donations.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+#if "DATABASE_URL" in os.environ:
+    #DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+#else:
+    #DATABASES = {
+        #"default": {
+           #"ENGINE": "django.db.backends.sqlite3",
+            #"NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        #}
+    #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
