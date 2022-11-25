19b09c08d2135cf588e01307db3ad3bc06bd671b
chellej80 <embarrequestrian@gmail.com>
2022-11-25 23:31:43 +0000

    deleted subscribe model

--- home/admin.py
+++ home/admin.py
@@ -1,14 +1,14 @@
 """Admin Imports"""
 from django import forms
 from django.contrib import admin
-from .models import Contact, Subscriber
+from .models import Contact
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('County', 'Town', 'Eircode', 'Description')
 
-admin.site.register(Subscriber)
+
 
 
 
