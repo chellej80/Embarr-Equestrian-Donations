bb5de012fd774b0dc5e1abc0653604b8249b936d
chellej80 <embarrequestrian@gmail.com>
2022-11-30 17:17:47 +0000

    push fix wh

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -59,9 +59,9 @@ class StripeWH_Handler:
         shipping_details = intent.shipping
         grand_total = round(stripe_charge.amount / 100, 2) # updated
         # Clean data in the shipping details
-        for field, value in shipping_details.address.items():
-            if value == "":
-                shipping_details.address[field] = None
+        #for field, value in shipping_details.address.items():
+            #if value == "":
+                #shipping_details.address[field] = None
 
         # Update profile information if save_info was checked
         profile = None
