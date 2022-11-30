d531c832feb4ed815560a481874d2985ff64ddf1
chellej80 <embarrequestrian@gmail.com>
2022-11-30 15:29:13 +0000

    update profile app field in WH

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -69,13 +69,13 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.default_phone_number = billing_details.phone
-                profile.default_country = billing_details.address.country
-                profile.default_postcode = billing_details.address.postal_code
-                profile.default_town_or_city = billing_details.address.city
-                profile.default_street_address1 = billing_details.address.line1
-                profile.default_street_address2 = billing_details.address.line2
-                profile.default_county = billing_details.address.state
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
                 profile.save()
 
         order_exists = False
