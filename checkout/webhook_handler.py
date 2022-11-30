5ce579d19313e5b85a038cef50e3523a7abfacd5
chellej80 <embarrequestrian@gmail.com>
2022-11-30 14:58:25 +0000

    updated webhook

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -113,7 +113,7 @@ class StripeWH_Handler:
                     full_name=shipping_details.name,
                     email=billing_details.email,
                     user_profile=profile,
-                    phone_number=shipping_details.phone,
+                    phone_number=billing_details.phone,
                     country=shipping_details.address.country,
                     postcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
