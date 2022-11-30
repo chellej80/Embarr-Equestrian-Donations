4a497640a90c2d59e15af678e94251c6f11768a9
chellej80 <embarrequestrian@gmail.com>
2022-11-30 17:23:26 +0000

    updated field in WH

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -56,7 +56,7 @@ class StripeWH_Handler:
         )
 
         billing_details = stripe_charge.billing_details # updated
-        shipping_details = intent.shipping
+        #shipping_details = intent.shipping
         grand_total = round(stripe_charge.amount / 100, 2) # updated
         # Clean data in the shipping details
         #for field, value in shipping_details.address.items():
