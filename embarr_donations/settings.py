a9cb867abbf3a82aff74c08c07953bb4e003b6bc
chellej80 <embarrequestrian@gmail.com>
2022-11-30 01:58:42 +0000

    updated email pw

--- embarr_donations/settings.py
+++ embarr_donations/settings.py
@@ -215,7 +215,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
     
