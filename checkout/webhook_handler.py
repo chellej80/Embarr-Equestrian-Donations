eafcd39154aa3db74eb501bd02de58cb083030c2
chellej80 <embarrequestrian@gmail.com>
2022-11-30 18:01:57 +0000

    updated phone number wh

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -85,7 +85,7 @@ class StripeWH_Handler:
                 order = Order.objects.get(
                     full_name__iexact=billing_details.name,
                     email__iexact=billing_details.email,
-                    #phone_number__iexact=shipping_details.phone,
+                    phone_number__iexact=shipping_details.phone,
                     #country__iexact=shipping_details.address.country,
                     #postcode__iexact=shipping_details.address.postal_code,
                     #town_or_city__iexact=shipping_details.address.city,
@@ -113,7 +113,7 @@ class StripeWH_Handler:
                     full_name=billing_details.name,
                     email=billing_details.email,
                     user_profile=profile,
-                    #phone_number=shipping_details.phone,
+                    phone_number=shipping_details.phone,
                     #country=shipping_details.address.country,
                     #postcode=shipping_details.address.postal_code,
                     #town_or_city=shipping_details.address.city,
