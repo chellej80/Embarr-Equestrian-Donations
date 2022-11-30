7cb5f2c8c8ff49f092bcc761392deec13f66a16f
chellej80 <embarrequestrian@gmail.com>
2022-11-30 02:20:36 +0000

    final update

--- embarr_donations/settings.py
+++ embarr_donations/settings.py
@@ -212,7 +212,7 @@ STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'Embarr@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -220,6 +220,6 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    #DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
     
