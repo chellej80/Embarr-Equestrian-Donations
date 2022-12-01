34487ca1e7285a92daa51061b6ad64506f5a2003
chellej80 <embarrequestrian@gmail.com>
2022-12-01 17:07:56 +0000

    updated wh if statment

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -123,15 +123,15 @@ class StripeWH_Handler:
                     original_cart=cart,
                     stripe_pid=pid,
                 )
-                #for item_id, item_data in json.loads(cart).items():
-                    #animal = Animal.objects.get(id=item_id)
-                    #if isinstance(item_data, int):
-                        #order_line_item = OrderLineItem(
-                            #order=order,
-                            #animal=animal,
-                            #quantity=item_data,
-                        #)
-                        #order_line_item.save()
+                for item_id, item_data in json.loads(cart).items():
+                    animal = Animal.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            animal=animal,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
                     #else:
                         #for size, quantity in item_data['items_by_size'].items():
                             #order_line_item = OrderLineItem(
