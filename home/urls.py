aa2c20384fc2180e6f91e094fc939e18ddef5a20
chellej80 <embarrequestrian@gmail.com>
2022-12-08 12:26:57 +0000

    updated urls in main app

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name='index'),
     path('about', views.about, name='about'),
     path('contact', views.contact_view, name='contact'),
 ]
