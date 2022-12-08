0289b0543fc4e315f2246023686bc4b114da5be8
chellej80 <embarrequestrian@gmail.com>
2022-12-08 12:53:59 +0000

    updated embarr urls

--- embarr_donations/urls.py
+++ embarr_donations/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
 from . import views
 
 urlpatterns = [
-    path("", include("home.urls")),
+    path("", include("home.urls"), name="home.urls"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls"), name="accounts.urls"),
     path("animals/", include("animals.urls")),
