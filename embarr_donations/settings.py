1e47c8735606093d00a7543035a02744724a6626
chellej80 <embarrequestrian@gmail.com>
2022-12-08 12:29:28 +0000

    updates settings

--- embarr_donations/settings.py
+++ embarr_donations/settings.py
@@ -112,7 +112,7 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "/index"
 
 WSGI_APPLICATION = "embarr_donations.wsgi.application"
 
